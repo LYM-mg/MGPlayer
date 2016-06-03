e8136b8e142f4aebc68ae87765ea5fe62c56ed98
ming <mg13750525922@sina.com>
2016-06-03 20:23:39 +0800

    初始化项目

---
+++ MGPlayer/MGPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MGPlayer
+//
+//  Created by ming on 16/6/3.
+//  Copyright © 2016年 ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
